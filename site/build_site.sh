4fe25b55c292962b7477b438677ffce30ab59381
Adrian Siwiec <adrian.siwiec@gmail.com>
2024-04-04 08:05:09 +0200

    Make site a bit more presentable

--- site/build_site.sh
+++ site/build_site.sh
@@ -3,8 +3,8 @@ mkdir site/out
 # emcc -I ./inc/ src/*.cpp test/main.cpp -o site/out/main.html --shell-file site/html_template/shell_minimal.html
 
 echo "Compiling emscripten_lib.cpp"
-emcc -I ./inc/ src/*.cpp site/emscripten_lib.cpp -o site/out/emscripten_lib.html -sEXPORTED_FUNCTIONS=_SolveFormulas -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --shell-file site/html_template/shell_minimal.html
+emcc -I ./inc/ src/*.cpp site/emscripten_lib.cpp -o site/out/sci.html -sEXPORTED_FUNCTIONS=_SolveFormulas -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --shell-file site/html_template/shell_minimal.html
 
 
-# echo "Copying site to website repo"
-# cp site/out/* ../AdrianSiwiec.github.io/sci/
+echo "Copying site to website repo"
+cp site/out/* ../AdrianSiwiec.github.io/sci/
