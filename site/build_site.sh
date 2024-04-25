cc989249aa0817a0411f7ad89f887844904d4af2
Adrian Siwiec <adrian.siwiec@gmail.com>
2024-04-25 06:19:59 +0200

    Small fixes to the site

--- site/build_site.sh
+++ site/build_site.sh
@@ -3,7 +3,7 @@ mkdir site/out
 # emcc -I ./inc/ src/*.cpp test/main.cpp -o site/out/main.html --shell-file site/html_template/shell_minimal.html
 
 echo "Compiling emscripten_lib.cpp"
-# emcc -O3 -I ./inc/ src/*.cpp site/emscripten_lib.cpp -o site/out/sci.html -sEXPORTED_FUNCTIONS=_SolveFormulas -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --shell-file site/html_template/sci.html -s MINIFY_HTML=0 -s ALLOW_MEMORY_GROWTH=1
+emcc -O3 -I ./inc/ src/*.cpp site/emscripten_lib.cpp -o site/out/sci.html -sEXPORTED_FUNCTIONS=_SolveFormulas -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --shell-file site/html_template/sci.html -s MINIFY_HTML=0 -s ALLOW_MEMORY_GROWTH=1
 echo "Compiling emscripten_lib.cpp for Label"
 emcc -O3 -I ./inc/ src/*.cpp site/emscripten_lib.cpp -o site/out/sci_label.html -sEXPORTED_FUNCTIONS=_SolveFormulas,_SolveFormulasLabel -sEXPORTED_RUNTIME_METHODS=ccall,cwrap --shell-file site/html_template/sci_label.html -s MINIFY_HTML=0 -s ALLOW_MEMORY_GROWTH=1
 
